95f63eec3f3be23a916125d12459072dc8153841
dojyorin <ota_droid@live.jp>
2023-08-02 15:51:51 +0900

    span to div.

--- src/core/layout/notify.vue
+++ src/core/layout/notify.vue
@@ -1,6 +1,6 @@
 <template>
     <v-snackbar v-for="notify in notifies" model-value position="fixed" location="top" z-index="3000" :color="notify.color" timeout="-1" v-bind="notifyTimeout(notify, 5000)">
-        <span>{{notify.message}}</span>
+        <div>{{notify.message}}</div>
 
         <template #actions>
             <v-btn ripple density="comfortable" icon="mdi-close" @click="notifyTimeout(notify, 0)"></v-btn>
--- src/core/page/third.vue
+++ src/core/page/third.vue
@@ -1,7 +1,7 @@
 <template>
     <v-img :height="height" class="text-center align-center text-white" gradient="45deg, #FF9F17, #FF4E0E">
-        <span v-if="mobile" class="text-h4">MOBILE</span>
-        <span v-else class="text-h2">DESKTOP</span>
+        <div v-if="mobile" class="text-h4">MOBILE</div>
+        <div v-else class="text-h2">DESKTOP</div>
     </v-img>
 </template>
 
